acb49c8c05a1ef8ffbcf75649d1c48c7bd7d7d23
Jonathan Fridja <jonyfridja@gmail.com>
2026-07-18 11:21:35 +0300

    chore: misc

--- apps/api/src/data-source.ts
+++ apps/api/src/data-source.ts
@@ -9,7 +9,7 @@ loadEnv();
 export const AppDataSource = new DataSource({
   type: "postgres",
   host: process.env.DB_HOST ?? "localhost",
-  port: Number(process.env.DB_PORT ?? 5433),
+  port: Number(process.env.DB_PORT ?? 5432),
   username: process.env.DB_USERNAME ?? "planner",
   password: process.env.DB_PASSWORD ?? "planner",
   database: process.env.DB_NAME ?? "planner",
